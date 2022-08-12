dae00f5f8dfdfd11eda4fbf91c9187565a714a69
Aleksej Chervjakov <a.chervjakov@corp.mail.ru>
2022-08-12 10:34:31 +0300

    main Привет дрг
    https://jira.mail.ru/browse/main

---
+++ app/src/main/java/com/sirius/hello_friend/ui/activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sirius.hello_friend.ui.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sirius.hello_friend.ui.theme.HelloFriendTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
